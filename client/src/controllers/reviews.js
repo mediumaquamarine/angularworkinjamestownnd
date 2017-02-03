d9f56096e4a9566868c0ba0ac2855fbe139d3298
Seth Koch <sethkoch1@gmail.com>
2017-02-03 14:07:41 -0600

    reviews are displaying on the page

--- client/src/controllers/reviews.js
+++ client/src/controllers/reviews.js
@@ -1,14 +1,21 @@
 (function() {
 
   angular.module('work')
-    .controller('ReviewController', ['$rootScope', 'authService', 'userdata', '$state', '$window', 'companydata', function($rootScope, authService, userdata, $state, $window, companydata) {
+    .controller('ReviewController', ['$rootScope', 'authService', 'userdata', '$state', '$window', 'companydata', '$http', '$sce', function($rootScope, authService, userdata, $state, $window, companydata, $http, $sce) {
 
 
       var vm = this;
       //grabs the name of the current company
       vm.company = $window.localStorage.company;
-      vm.display = "Be the first to review";
+      vm.reviews = vm.reviews || "Be the first to review";
+      vm.trustAsHtml = $sce.trustAsHtml;
 
+      $http.post('api/reviews', {companyName: vm.company})
+        .then(function(res) {
+          vm.reviews = res.data.reviews;
+          console.log(res.data.authors);
+          console.log(res.data.reviews);
+        })
     }]);
 
 })();
